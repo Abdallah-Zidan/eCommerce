7c1f0f1a6b7c7f9b1f9233618ab4bb669e9e586b
Fatma Mohammed <gfatmamohammed@gmail.com>
2020-03-12 00:27:12 +0200

    filter by category working but not as wanted :/

--- js/script.js
+++ js/script.js
@@ -61,16 +61,72 @@ function previousPage() {
 }
 
 
+window.onload = function() {
+    clearChecks();
+    getProducts(1);
+  };
 
 
 
-// function check() {
-//     document.getElementById("next").disabled = currentPage == numberOfPages ? true : false;
-//     document.getElementById("previous").disabled = currentPage == 1 ? true : false;
-//     document.getElementById("first").disabled = currentPage == 1 ? true : false;
-//     document.getElementById("last").disabled = currentPage == numberOfPages ? true : false;
-// }
+
+///////////filter by category
+
+function filterByCat(category){
+    fetch('https://afternoon-falls-30227.herokuapp.com/api/v1/products/?category='+category)
+    .then((res)=>res.json())
+    .then((data)=>{
+        let output=""
+        data.data.forEach(product => {
+            output +=`
+            <div class="col-md-4">
+            <div class="single-product-wrapper">
+                <!-- Product Image -->
+                <div class="product-img ">
+                    <img class="img-fluid" src="${product.ProductPicUrl}" alt="" id="productImg">
+                </div>
+
+                <!-- Product Description -->
+                <div class="product-description d-flex align-items-center justify-content-between">
+                    <!-- Product Meta Data -->
+                    <div class="product-meta-data">
+                        <div class="line"></div>
+                        <p class="product-price">${product.CurrencyCode} ${product.Price}</p>
+                            <h6>${product.Name}</h6>
+                        
+                    </div>
+                    <!-- Cart -->
+                    <div class="ratings-cart text-right">
+                        <div class="cart">
+                            <a href="cart.html" data-toggle="tooltip" data-placement="left" title="Add to Cart"><img src="img/core-img/bag.png" alt=""></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>`
+
+                document.getElementById("postPlace").innerHTML =output;
+        });
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
+
+    
+}
+
+
+$("input[type='checkbox']").change(function() {
+    console.log("checked");
+    if($(this).is (':checked')) {
+        filterByCat($(this).attr("name"));
+    }
+});
 
 
 
-getProducts(1);
+/////////////////////////clear checks
+function clearChecks(){
+    if($("input[type='checkbox']").is (':checked'))
+    { $(this).prop('checked', false);
+        }
+}
