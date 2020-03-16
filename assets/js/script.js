f33ef117d00535bdb7ae066022982d2113e673cd
Fatma Mohammed <gfatmamohammed@gmail.com>
2020-03-16 14:46:28 +0200

    fix search

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,13 @@ let lastPage = 13;
 let currentPage = 1;
 
 ///////////////// fetch data from api
-function getProducts(page){
-    fetch('https://afternoon-falls-30227.herokuapp.com/api/v1/products/?page='+page)
-    .then((res)=>res.json())
-    .then((data)=>{
-        let output=""
-        data.data.forEach(product => {
-            output +=`
+function getProducts(page) {
+    fetch('https://afternoon-falls-30227.herokuapp.com/api/v1/products/?page=' + page)
+        .then((res) => res.json())
+        .then((data) => {
+            let output = ""
+            data.data.forEach(product => {
+                output += `
             <div class="col-md-4">
             <div class="single-product-wrapper">
                 <!-- Product Image -->
@@ -37,17 +37,17 @@ function getProducts(page){
             </div>
             </div>`
 
-                document.getElementById("postPlace").innerHTML =output;
-            // console.log("category: "+product.Category);
-            // console.log(product.SupplierName);
-            
-            
-        });
+                document.getElementById("postPlace").innerHTML = output;
+                // console.log("category: "+product.Category);
+                // console.log(product.SupplierName);
 
-    })
-    .catch((err)=>{
-        console.log(err);
-    });
+
+            });
+
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 }
 
 
@@ -56,34 +56,36 @@ function nextPage() {
     currentPage += 1;
     getProducts(currentPage);
 }
+
 function previousPage() {
     currentPage -= 1;
     getProducts(currentPage);
 }
 
 
-window.onload = function() {
+window.onload = function () {
     clearChecks();
     getProducts(1);
-  };
+};
 
 
 
 
 ///////////filter by category only
 
-function filterByCat(category){
-    fetch('https://afternoon-falls-30227.herokuapp.com/api/v1/products/?category='+category)
-    .then((res)=>res.json())
-    .then((data)=>{
-        let output=""
-        data.data.forEach(product => {
-            output +=`
+function filterByCat(category) {
+    fetch('https://afternoon-falls-30227.herokuapp.com/api/v1/products/?category=' + category)
+        .then((res) => res.json())
+        .then((data) => {
+            let output = ""
+            data.data.forEach(product => {
+                output += `
             <div class="col-md-4">
             <div class="single-product-wrapper">
                 <!-- Product Image -->
-                <div class="product-img ">
-                    <img class="img-fluid" src="${product.ProductPicUrl}" alt="" id="productImg">
+                <div class="product-img" pid="${product.ProductId}" data-target="#exampleModalCenteredScrollable"
+                data-toggle="modal" class="btn btn-show">
+                    <img class="img-fluid" src="${product.ProductPicUrl}" alt="" id="productImg" onClick="getRequest('${product.ProductId}')">
                 </div>
 
                 <!-- Product Description -->
@@ -91,34 +93,34 @@ function filterByCat(category){
                     <!-- Product Meta Data -->
                     <div class="product-meta-data">
                         <div class="line"></div>
-                        <p class="product-price">${product.CurrencyCode} ${product.Price}</p>
+                        <p class="product-price"> ${product.Price} <span style="font-size:17px;">${product.CurrencyCode}<span></p>
                             <h6>${product.Name}</h6>
                         
                     </div>
                     <!-- Cart -->
                     <div class="ratings-cart text-right">
                         <div class="cart">
-                            <a href="cart.html" data-toggle="tooltip" data-placement="left" title="Add to Cart"><img src="img/core-img/bag.png" alt=""></a>
+                            <a  onClick="addItemToLocalStorage(${product.ProductId}, ${product.Quantity})" href="" data-toggle="tooltip" data-placement="left" title="Add to Cart"><img src="assets/images/bag.png" alt=""></a>
                         </div>
                     </div>
                 </div>
             </div>
             </div>`
 
-                document.getElementById("postPlace").innerHTML =output;
+                document.getElementById("postPlace").innerHTML = output;
+            });
+        })
+        .catch((err) => {
+            console.log(err);
         });
-    })
-    .catch((err)=>{
-        console.log(err);
-    });
 
-    
+
 }
 
-$("input.cat").change(function() {
-    
-    if($(this).is (':checked')) {
-        
+$("input.cat").change(function () {
+
+    if ($(this).is(':checked')) {
+
         filterByCat($(this).attr("name"));
     }
     clearChecks();
@@ -127,18 +129,19 @@ $("input.cat").change(function() {
 
 
 ///////////////////////////////////filter by supplier
-function filterBySup(sup){
-    fetch('https://afternoon-falls-30227.herokuapp.com/api/v1/products/?supplier='+sup)
-    .then((res)=>res.json())
-    .then((data)=>{
-        let output=""
-        data.data.forEach(product => {
-            output +=`
+function filterBySup(sup) {
+    fetch('https://afternoon-falls-30227.herokuapp.com/api/v1/products/?supplier=' + sup)
+        .then((res) => res.json())
+        .then((data) => {
+            let output = ""
+            data.data.forEach(product => {
+                output += `
             <div class="col-md-4">
             <div class="single-product-wrapper">
                 <!-- Product Image -->
-                <div class="product-img ">
-                    <img class="img-fluid" src="${product.ProductPicUrl}" alt="" id="productImg">
+                <div class="product-img" pid="${product.ProductId}" data-target="#exampleModalCenteredScrollable"
+                data-toggle="modal" class="btn btn-show">
+                    <img class="img-fluid" src="${product.ProductPicUrl}" alt="" id="productImg" onClick="getRequest('${product.ProductId}')">
                 </div>
 
                 <!-- Product Description -->
@@ -146,34 +149,34 @@ function filterBySup(sup){
                     <!-- Product Meta Data -->
                     <div class="product-meta-data">
                         <div class="line"></div>
-                        <p class="product-price">${product.CurrencyCode} ${product.Price}</p>
+                        <p class="product-price"> ${product.Price} <span style="font-size:17px;">${product.CurrencyCode}<span></p>
                             <h6>${product.Name}</h6>
                         
                     </div>
                     <!-- Cart -->
                     <div class="ratings-cart text-right">
                         <div class="cart">
-                            <a href="cart.html" data-toggle="tooltip" data-placement="left" title="Add to Cart"><img src="img/core-img/bag.png" alt=""></a>
+                            <a  onClick="addItemToLocalStorage(${product.ProductId}, ${product.Quantity})" href="" data-toggle="tooltip" data-placement="left" title="Add to Cart"><img src="assets/images/bag.png" alt=""></a>
                         </div>
                     </div>
                 </div>
             </div>
             </div>`
 
-                document.getElementById("postPlace").innerHTML =output;
+                document.getElementById("postPlace").innerHTML = output;
+            });
+        })
+        .catch((err) => {
+            console.log(err);
         });
-    })
-    .catch((err)=>{
-        console.log(err);
-    });
 
-    
+
 }
 
-$("input.sup").change(function() {
-    
-    if($(this).is (':checked')) {
-        
+$("input.sup").change(function () {
+
+    if ($(this).is(':checked')) {
+
         filterBySup($(this).attr("name"));
     }
     clearChecks();
@@ -183,29 +186,38 @@ $("input.sup").change(function() {
 
 
 /////////////////////////clear checks
-function clearChecks(){
-    if($("input[type='checkbox']").is(':checked'))
-    {   console.log("enterd if")
+function clearChecks() {
+    if ($("input[type='checkbox']").is(':checked')) {
+        console.log("enterd if")
         $("input[type='checkbox']").prop('checked', false);
-        
-     }
+
+    }
 }
 
 
 //////////////////////////////////Search 
-function Search(){
+
+$("#searchBtn").click(() => {
+    Search();
+})
+
+
+function Search() {
     let text = $("#textSearch")[0].value;
-    fetch('https://afternoon-falls-30227.herokuapp.com/api/v1/products/?q='+text)
-    .then((res)=>res.json())
-    .then((data)=>{
-        let output=""
-        data.data.forEach(product => {
-            output +=`
+    console.log(text);
+    console.log('https://afternoon-falls-30227.herokuapp.com/api/v1/products/?q=' + text);
+    fetch('https://afternoon-falls-30227.herokuapp.com/api/v1/products/?q=' + text)
+        .then((res) => res.json())
+        .then((data) => {
+            let output = ""
+            data.data.forEach(product => {
+                output += `
             <div class="col-md-4">
             <div class="single-product-wrapper">
                 <!-- Product Image -->
-                <div class="product-img ">
-                    <img class="img-fluid" src="${product.ProductPicUrl}" alt="" id="productImg">
+                <div class="product-img" pid="${product.ProductId}" data-target="#exampleModalCenteredScrollable"
+                data-toggle="modal" class="btn btn-show">
+                    <img class="img-fluid" src="${product.ProductPicUrl}" alt="" id="productImg" onClick="getRequest('${product.ProductId}')">
                 </div>
 
                 <!-- Product Description -->
@@ -213,25 +225,28 @@ function Search(){
                     <!-- Product Meta Data -->
                     <div class="product-meta-data">
                         <div class="line"></div>
-                        <p class="product-price">${product.CurrencyCode} ${product.Price}</p>
+                        <p class="product-price"> ${product.Price} <span style="font-size:17px;">${product.CurrencyCode}<span></p>
                             <h6>${product.Name}</h6>
                         
                     </div>
                     <!-- Cart -->
                     <div class="ratings-cart text-right">
                         <div class="cart">
-                            <a href="cart.html" data-toggle="tooltip" data-placement="left" title="Add to Cart"><img src="img/core-img/bag.png" alt=""></a>
+                            <a  onClick="addItemToLocalStorage(${product.ProductId}, ${product.Quantity})" href="" data-toggle="tooltip" data-placement="left" title="Add to Cart"><img src="assets/images/bag.png" alt=""></a>
                         </div>
                     </div>
                 </div>
             </div>
             </div>`
+
+                document.getElementById("postPlace").innerHTML = output;
+                
+            });
+            console.log($("#textSearch")[0]);
             $("#textSearch")[0].value="";
-                document.getElementById("postPlace").innerHTML =output;
+        })
+        .catch((err) => {
+            console.log(err);
         });
-    })
-    .catch((err)=>{
-        console.log(err);
-    });
-    
+
 }
