4b4a3e86cc4cd5e274c692d71d1c36202aa95537
Fatma Mohammed <gfatmamohammed@gmail.com>
2020-03-16 03:18:07 +0200

    add search

--- assets/js/script.js
+++ assets/js/script.js
@@ -190,3 +190,48 @@ function clearChecks(){
         
      }
 }
+
+
+//////////////////////////////////Search 
+function Search(){
+    let text = $("#textSearch")[0].value;
+    fetch('https://afternoon-falls-30227.herokuapp.com/api/v1/products/?q='+text)
+    .then((res)=>res.json())
+    .then((data)=>{
+        let output=""
+        data.data.forEach(product => {
+            output +=`
+            <div class="col-md-4">
+            <div class="single-product-wrapper">
+                <!-- Product Image -->
+                <div class="product-img ">
+                    <img class="img-fluid" src="${product.ProductPicUrl}" alt="" id="productImg">
+                </div>
+
+                <!-- Product Description -->
+                <div class="product-description d-flex align-items-center justify-content-between">
+                    <!-- Product Meta Data -->
+                    <div class="product-meta-data">
+                        <div class="line"></div>
+                        <p class="product-price">${product.CurrencyCode} ${product.Price}</p>
+                            <h6>${product.Name}</h6>
+                        
+                    </div>
+                    <!-- Cart -->
+                    <div class="ratings-cart text-right">
+                        <div class="cart">
+                            <a href="cart.html" data-toggle="tooltip" data-placement="left" title="Add to Cart"><img src="img/core-img/bag.png" alt=""></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>`
+            $("#textSearch")[0].value="";
+                document.getElementById("postPlace").innerHTML =output;
+        });
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
+    
+}
